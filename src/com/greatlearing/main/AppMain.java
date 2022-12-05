3902524847e09e068e40aac4db999126860b3b90
BHARGAV REDDY R <116778632+BhargavReddyR@users.noreply.github.com>
2022-12-05 12:25:12 +0530

    Update AppMain.java

--- src/com/greatlearing/main/AppMain.java
+++ src/com/greatlearing/main/AppMain.java
@@ -8,56 +8,61 @@ import com.greatlearing.service.CredentialsService;
 
 public class AppMain {
 	public static void main(String[] args) {
-					
-		Employee demoEmployee = new Employee("John", "Adam");
+		
+		
+		Employee demoEmployee = new Employee("Bhargav", "Ajay");
 		ICredentials credentialsService = new CredentialsService();
 		
-		System.out.println("Please choose the department from the following");
-		System.out.println("1. Thechnical");
+		System.out.println("Please choose the department from the following options");
+		System.out.println("1. Technical");
 		System.out.println("2. Admin");
-		System.out.println("3. Human Resource");
+		System.out.println("3. Human Resources");
 		System.out.println("4. Legal");
 		
 		Scanner userInput = new Scanner(System.in);
 		int option = userInput.nextInt();
-			
+		
 		String generatedEmail = null;
 		String generatedPassword = null;
-			
+		
 		switch(option) {
-			
 			case 1: {
-				generatedEmail = credentialsService.generateEmailAddress(demoEmployee.getFirstName().toLowerCase(),demoEmployee.getLastName().toLowerCase(),"tech");
+				//dept is technical, short form is tech
+				generatedEmail = credentialsService.generateEmailAddress(demoEmployee.getFirstName().toLowerCase(),
+											demoEmployee.getLastName().toLowerCase(), "tech");
 				generatedPassword = credentialsService.generatePassword();
 				break;
 			}
 			case 2: {
-				generatedEmail = credentialsService.generateEmailAddress(demoEmployee.getFirstName().toLowerCase(),demoEmployee.getLastName().toLowerCase(),"adm");
+				//dept is Admin, short form is adm
+				generatedEmail = credentialsService.generateEmailAddress(demoEmployee.getFirstName().toLowerCase(),
+											demoEmployee.getLastName().toLowerCase(), "adm");
 				generatedPassword = credentialsService.generatePassword();
 				break;
 			}
 			case 3: {
-				generatedEmail = credentialsService.generateEmailAddress(demoEmployee.getFirstName().toLowerCase(),demoEmployee.getLastName().toLowerCase(),"hr");
+				//dept is HR, short form is hr
+				generatedEmail = credentialsService.generateEmailAddress(demoEmployee.getFirstName().toLowerCase(),
+											demoEmployee.getLastName().toLowerCase(), "hr");
 				generatedPassword = credentialsService.generatePassword();
 				break;
 			}
 			case 4: {
-				generatedEmail = credentialsService.generateEmailAddress(demoEmployee.getFirstName().toLowerCase(),demoEmployee.getLastName().toLowerCase(),"lg");
+				//dept is Legal, short form is lg
+				generatedEmail = credentialsService.generateEmailAddress(demoEmployee.getFirstName().toLowerCase(),
+											demoEmployee.getLastName().toLowerCase(), "lg");
 				generatedPassword = credentialsService.generatePassword();
 				break;
 			}
 			default : {
-				System.out.println("Enter a valid department optiom");
-			}
-			
-			demoEmployee.setEmail(generatedEmail);
-			demoEmployee.setPassword(generatedPassword);
-			credentialsService.showCredentials(demoEmployee);
-			userInput.close();	
-				
+				System.out.println("Enter a valid department option");
 			}
 		}
-	
-	
 		
-}		
+		demoEmployee.setEmail(generatedEmail);
+		demoEmployee.setPassword(generatedPassword);
+		credentialsService.showCredentials(demoEmployee);
+		
+		userInput.close();
+	}
+}
